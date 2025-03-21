6632953f9f2187547acdd92321ac52ddf786900d
Xurshid97 <xurshedrabo@gmail.com>
2025-03-21 21:21:50 +0100

    pwa full screen, and notification

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,29 @@
 import { Link } from "react-router-dom";
 
 export default function Home() {    
+    const showNotification = () => {
+        if ("Notification" in window) {
+            if (Notification.permission === "granted") {
+                new Notification("You are the best!");
+            } else if (Notification.permission !== "denied") {
+                Notification.requestPermission().then(permission => {
+                    if (permission === "granted") {
+                        new Notification("You are the best!");
+                    }
+                });
+            }
+        } else {
+            console.log("Notifications are not supported in this browser.");
+        }
+    };
+
     return (
         <div>
-        <h1>Home</h1>
-        <p>This is the home
-        page.</p>
-        <Link to="/about">About</Link>
+            <h1>Home</h1>
+            <p>This is the home page.</p>
+            <Link to="/about">About</Link>
+            <br />
+            <button onClick={showNotification}>Click me</button>
         </div>
-    )
-    }
+    );
+}
